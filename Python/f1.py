2a0fbf1728f929e1e2c46b67f464f55d862b7d5b
kaustavsenguptagithub1977 <senguptakaustav2000@gmail.com>
2021-05-31 18:01:34 +0530

    My first commit and push to cloud directory

---
+++ Python/f1.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+c = a + b
+print(c 
+
+i = 0
+for i in range((1,11):
+    if i == 0:
+    print('Zero')
+    elif i>0:
+    print("The series of numbers are: ", i)
+       i += 1
+    
